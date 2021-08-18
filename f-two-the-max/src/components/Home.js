dcc2feca7622d185d4936fcbd72fe94083be0d28
young <badfriend87@hotmail.com>
2021-08-17 22:02:38 -0700

    remove unnecessary variable

--- f-two-the-max/src/components/Home.js
+++ f-two-the-max/src/components/Home.js
@@ -16,25 +16,6 @@ const sections = [
   { title: 'Personnel', url: '#' }
 ];
 
-const featuredPosts = [
-  {
-    title: 'Featured post',
-    date: 'Nov 12',
-    description:
-      'This is a wider card with supporting text below as a natural lead-in to additional content.',
-    image: 'https://source.unsplash.com/random',
-    imageText: 'Image Text',
-  },
-  {
-    title: 'Post title',
-    date: 'Nov 11',
-    description:
-      'This is a wider card with supporting text below as a natural lead-in to additional content.',
-    image: 'https://source.unsplash.com/random',
-    imageText: 'Image Text',
-  },
-];
-
 const Home = () => {
   return (
     <React.Fragment>
